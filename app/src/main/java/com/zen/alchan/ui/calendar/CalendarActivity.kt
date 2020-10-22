f629040c0fe5524bd759fe58bd8d8a565cc1c828
Zen Dharma <zendharma10@gmail.com>
2020-10-22 17:38:43 +0700

    Calendar added

--- app/src/main/java/com/zen/alchan/ui/calendar/CalendarActivity.kt
+++ app/src/main/java/com/zen/alchan/ui/calendar/CalendarActivity.kt
@@ -1,30 +1,36 @@
 package com.zen.alchan.ui.calendar
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
-import androidx.core.util.Pair
-import com.google.android.material.datepicker.MaterialDatePicker
 import com.zen.alchan.R
+import com.zen.alchan.helper.changeStatusBarColor
+import com.zen.alchan.helper.enums.BrowsePage
 import com.zen.alchan.helper.enums.ResponseStatus
-import com.zen.alchan.helper.pojo.DateItem
+import com.zen.alchan.helper.utils.AndroidUtility
+import com.zen.alchan.helper.utils.DialogUtility
 import com.zen.alchan.ui.base.BaseActivity
+import com.zen.alchan.ui.browse.BrowseActivity
 import kotlinx.android.synthetic.main.activity_calendar.*
+import kotlinx.android.synthetic.main.layout_empty.*
 import kotlinx.android.synthetic.main.layout_loading.*
 import kotlinx.android.synthetic.main.layout_toolbar.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.util.*
 
 class CalendarActivity : BaseActivity() {
 
     private val viewModel by viewModel<CalendarViewModel>()
 
     private lateinit var dateAdapter: CalendarDateRvAdapter
+    private lateinit var scheduleAdapter: CalendarScheduleRvAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
 
+        changeStatusBarColor(AndroidUtility.getResValueFromRefAttr(this, R.attr.themeCardColor))
         setSupportActionBar(toolbarLayout)
         supportActionBar?.apply {
             title = getString(R.string.calendar)
@@ -43,15 +49,36 @@ class CalendarActivity : BaseActivity() {
             when (it.responseStatus) {
                 ResponseStatus.LOADING -> loadingLayout.visibility = View.VISIBLE
                 ResponseStatus.SUCCESS -> {
-                    loadingLayout.visibility = View.GONE
+                    if (!viewModel.hasNextPage) {
+                        loadingLayout.visibility = View.GONE
+                        return@Observer
+                    }
+
+                    viewModel.hasNextPage = it.data?.page?.pageInfo?.hasNextPage ?: false
+                    viewModel.page += 1
+                    viewModel.isInit = true
+                    viewModel.scheduleList.addAll(it.data?.page?.airingSchedules?.filterNotNull() ?: listOf())
+
+                    if (viewModel.hasNextPage) {
+                        viewModel.getAiringSchedule()
+                    } else {
+                        loadingLayout.visibility = View.GONE
+                        assignScheduleAdapter()
+                    }
                 }
                 ResponseStatus.ERROR -> {
                     loadingLayout.visibility = View.GONE
+                    DialogUtility.showToast(this, it.message)
+                    assignScheduleAdapter()
                 }
             }
         })
 
-        viewModel.getAiringSchedule()
+        if (!viewModel.isInit) {
+            viewModel.getAiringSchedule()
+        } else {
+            assignScheduleAdapter()
+        }
     }
 
     private fun initLayout() {
@@ -61,11 +88,10 @@ class CalendarActivity : BaseActivity() {
             dateAdapter = assignDateAdapter()
             dateRecyclerView.adapter = dateAdapter
 
+            viewModel.hasNextPage = true
+            viewModel.page = 1
             viewModel.getAiringSchedule()
         }
-
-
-
     }
 
     private fun assignDateAdapter(): CalendarDateRvAdapter {
@@ -76,8 +102,63 @@ class CalendarActivity : BaseActivity() {
                 viewModel.dateList.forEach { it.isSelected = false }
                 viewModel.dateList[position].isSelected = true
                 dateAdapter.notifyDataSetChanged()
+                assignScheduleAdapter()
             }
         })
+    }
+
+    private fun assignScheduleAdapter() {
+        viewModel.filterList()
+
+        scheduleAdapter = CalendarScheduleRvAdapter(this, viewModel.filteredList, object : CalendarScheduleRvAdapter.CalendarScheduleListener {
+            override fun openMedia(id: Int) {
+                val intent = Intent(this@CalendarActivity, BrowseActivity::class.java)
+                intent.putExtra(BrowseActivity.TARGET_PAGE, BrowsePage.ANIME.name)
+                intent.putExtra(BrowseActivity.LOAD_ID, id)
+                startActivity(intent)
+            }
+        })
+        scheduleRecyclerView.adapter = scheduleAdapter
+
+        if (viewModel.filteredList.isNullOrEmpty()) {
+            scheduleRecyclerView.visibility = View.GONE
+            emptyLayout.visibility = View.VISIBLE
+        } else {
+            scheduleRecyclerView.visibility = View.VISIBLE
+            emptyLayout.visibility = View.GONE
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_filter, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.itemFilter) {
+            val dialog = CalendarFilterBottomSheetDialog()
+            dialog.setListener(object : CalendarFilterBottomSheetDialog.CalendarFilterListener {
+                override fun passFilterData(
+                    showOnlyInList: Boolean,
+                    showOnlyCurrentSeason: Boolean,
+                    showAdultContent: Boolean
+                ) {
+                    viewModel.showOnlyOnWatchingAndPlanning = showOnlyInList
+                    viewModel.showOnlyCurrentSeason = showOnlyCurrentSeason
+                    viewModel.showAdult = showAdultContent
+                    assignScheduleAdapter()
+                }
+            })
+
+            val bundle = Bundle()
+            bundle.putBoolean(CalendarFilterBottomSheetDialog.SHOW_ONLY_IN_LIST, viewModel.showOnlyOnWatchingAndPlanning)
+            bundle.putBoolean(CalendarFilterBottomSheetDialog.SHOW_ONLY_CURRENT_SEASON, viewModel.showOnlyCurrentSeason)
+            bundle.putBoolean(CalendarFilterBottomSheetDialog.SHOW_ADULT_CONTENT, viewModel.showAdult)
+
+            dialog.arguments = bundle
+            dialog.show(supportFragmentManager, null)
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/com/zen/alchan/ui/calendar/CalendarViewModel.kt
+++ app/src/main/java/com/zen/alchan/ui/calendar/CalendarViewModel.kt
@@ -3,6 +3,9 @@ package com.zen.alchan.ui.calendar
 import androidx.lifecycle.ViewModel
 import com.zen.alchan.data.repository.SearchRepository
 import com.zen.alchan.helper.pojo.DateItem
+import com.zen.alchan.helper.utils.Utility
+import okio.Utf8
+import type.MediaListStatus
 import type.MediaSeason
 import java.util.*
 import kotlin.collections.ArrayList
@@ -12,8 +15,14 @@ class CalendarViewModel(private val searchRepository: SearchRepository) : ViewMo
     var page = 1
     var hasNextPage = true
     var isInit = false
-    var scheduleList = ArrayList<AiringScheduleQuery.AiringSchedule?>()
+    var scheduleList = ArrayList<AiringScheduleQuery.AiringSchedule>()
+    var filteredList = ArrayList<AiringScheduleQuery.AiringSchedule>()
+
     var dateList = ArrayList<DateItem>()
+
+    var showOnlyOnWatchingAndPlanning = true
+    var showAdult = false
+    var showOnlyCurrentSeason = false
 
     val airingScheduleResponse by lazy {
         searchRepository.airingScheduleResponse
@@ -42,5 +51,30 @@ class CalendarViewModel(private val searchRepository: SearchRepository) : ViewMo
             calendar.add(Calendar.DAY_OF_MONTH, 1)
         }
         dateList[0].isSelected = true
+    }
+
+    fun filterList() {
+        filteredList.clear()
+
+        val startDate = dateList.find { it.isSelected }?.dateTimestamp ?: dateList[0].dateTimestamp
+        val endDate = startDate + (24 * 3600 * 1000)
+
+        val tempList = scheduleList.filter { it.airingAt * 1000L in startDate until endDate }
+
+        tempList.forEach {
+            if (showOnlyOnWatchingAndPlanning && !(it.media?.mediaListEntry?.status == MediaListStatus.CURRENT || it.media?.mediaListEntry?.status == MediaListStatus.PLANNING)) {
+                return@forEach
+            }
+
+            if (!showAdult && it.media?.isAdult == true) {
+                return@forEach
+            }
+
+            if (showOnlyCurrentSeason && (it.media?.season != Utility.getCurrentSeason() || it.media.seasonYear != Utility.getCurrentYear())) {
+                return@forEach
+            }
+
+            filteredList.add(it)
+        }
     }
 }
