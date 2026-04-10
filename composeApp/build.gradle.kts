8a446a57f3e1d71278cbcac8ce88b7aaa229509f
Zen Dharma <zendharma10@gmail.com>
2026-04-10 23:12:47 +0800

    Add debug tag to debug APK

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -98,6 +98,9 @@ android {
         }
     }
     buildTypes {
+        getByName("debug") {
+            applicationIdSuffix = ".debug"
+        }
         getByName("release") {
             isMinifyEnabled = false
         }
